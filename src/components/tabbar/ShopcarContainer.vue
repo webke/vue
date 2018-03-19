08776c87b0a8a7a8c71f6087355d97646bfeb23e
webke <383469145@qq.com>
2018-03-19 14:14:24 +0800

    first commit

---
+++ src/components/tabbar/ShopcarContainer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>ShopcarContainer</h3>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style  scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+// Vue.use(Router)
+
+// export default new Router({
+//     routes: [{
+//         path: '/',
+//         name: 'HelloWorld',
+//         component: HelloWorld
+//     }]
+// })
+
+//---------------------------------------------------------------------------------
+
+import Vue from 'vue'
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 导入对应的路由组件
+import HomeContainer from '../components/tabbar/HomeContainer.vue'
+import MemberContainer from '../components/tabbar/MemberContainer.vue'
+import ShopcarContainer from '../components/tabbar/ShopcarContainer.vue'
+import SearchContainer from '../components/tabbar/SearchContainer.vue'
+
+// 3. 创建路由对象
+var router = new VueRouter({
+    routes: [ // 配置路由规则
+        { path: '/', redirect: '/home' },
+        { path: '/home', component: HomeContainer },
+        { path: '/member', component: MemberContainer },
+        { path: '/shopcar', component: ShopcarContainer },
+        { path: '/search', component: SearchContainer }
+    ],
+    linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
+})
+
+// 把路由对象暴露出去
+export default router
